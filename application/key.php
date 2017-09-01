f1ec4b8543cea9bb8b2f3a9c836bf4aaa0a0aed2
grj <294807922@qq.com>
2017-09-01 16:32:32 +0800

    .gitignore

---
+++ application/key.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: GUO
+ * Date: 2017/9/1
+ * Time: 16:07
+ */
+/**
+ * 微信公众号appid
+ * @return string
+ */
+function wx_appid(){
+    return 'wx718b48048450240e';
+}
+
+/**
+ * 微信公众号appsecret
+ * @return string
+ */
+function wx_appsecret(){
+    return '776545aaguohong518aijia518202717';
+}
